6e8a2e69297c7c27f9a2aa70afe748f8b03f70b5
toru <toru@1f37519a-7ad4-497f-bc20-5d7ca8e04135>
2008-03-25 08:27:42 +0000

    added cont-cgi
    git-svn-id: svn+ssh://torus.jp/home/toru/svn/misc/cont-cgi@3 1f37519a-7ad4-497f-bc20-5d7ca8e04135

---
+++ cont.scm
@@ -0,0 +1,48 @@
+#!/usr/local/bin/gosh
+
+(define *COUNT* 0)
+
+(define *cont-vec* (make-vector 10))
+
+(define (do-continuation index . args)
+  #?=index
+  (apply (vector-ref *cont-vec* index) args))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; macros
+
+(define-macro (make-cont proc)
+  (let1 index *COUNT*
+    (inc! *COUNT*)
+    (vector-set! *cont-vec* index (eval proc ()))
+    `(lambda x 
+       (print `(cont.scm ,,index ,@x)))))
+
+(define-macro (cont-lambda args . body)
+  `(make-cont (lambda ,args ,@body)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; definitions
+
+(define exp-part
+  (cont-lambda
+   (x max-n n p prev-fact-n)
+   (if (> n max-n)
+       ((cont-lambda (res) (print "e = " res)) p)
+       (let* ((fact-n (* n prev-fact-n))
+	      (v (/. (expt x n) fact-n)))
+	 (exp-part x max-n (+ n 1) (+ p v) fact-n)))))
+
+(define calc-e
+  (cont-lambda () (exp-part 1 4 1 0 1)))
+
+;; (define output-result
+;;   (cont-lambda (res) (print "e = " res)))
+
+(define (main cmdline)
+  (let1 args (cdr cmdline)
+    (if (null? args)
+	(calc-e)
+	(let1 arg-list (map (cut with-input-from-string <> read) args)
+	  (apply do-continuation arg-list)
+	  ))))
