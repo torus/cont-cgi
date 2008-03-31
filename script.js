8f4505deaf636f59cfb8ee646eb421b31a2c47b2
toru <toru@1f37519a-7ad4-497f-bc20-5d7ca8e04135>
2008-03-31 05:16:56 +0000

    show tasks
    git-svn-id: svn+ssh://torus.jp/home/toru/svn/misc/cont-cgi@19 1f37519a-7ad4-497f-bc20-5d7ca8e04135

--- script.js
+++ script.js
@@ -35,6 +35,11 @@ xmlhttp.onreadystatechange = function() {
 	var dom = xmlhttp.responseXML;
 	var nodes = dom.documentElement.childNodes;
 
+	var dest = document.getElementById ("main");
+	debug_out ("dest: " + dest);
+	var ul = document.createElement ("ul");
+	dest.appendChild (ul);
+
 	if (nodes.length > 0) {
 	    debug_out (nodes.length);
 	    for (var i = 0; i < nodes.length; i ++) {
@@ -42,15 +47,19 @@ xmlhttp.onreadystatechange = function() {
 		var cont = nodes[i].firstChild.nodeValue;
 		debug_out (i + ": " + cont);
 
-		gen_task (cont);
+		var li = document.createElement ("li");
+		ul.appendChild (li);
+
+		gen_task (cont, li);
 	    }
 	}
     }
 };
 xmlhttp.send(null);
 
-function gen_task (cont) {
+function gen_task (cont, target_elem) {
     var xmlhttp = gen_xmlhttp ();
+
     xmlhttp.open ("GET", "./index.cgi?p=" + cont);
     xmlhttp.onreadystatechange = function () {
 	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -59,20 +68,39 @@ function gen_task (cont) {
 
 	    debug_out ("nodes: " + nodes);
 
-	    var elems = [];
-	    (function (i) {
-		if (i < nodes.length) {
-		    if (nodes[i].nodeType == 1) {
-			elems.push (nodes[i]);
-		    }
-		    arguments.callee (i + 1);
-		} else {
-		    return;
-		}
-	    }) (0);
+	    var elems = filter_element_nodes (nodes);
 
 	    debug_out ("elems: " + elems);
+
+	    var text = document.createTextNode (elems[0].firstChild.nodeValue);
+	    target_elem.appendChild (text);
+
+	    var text_cancel = document.createTextNode ("cancel");
+	    var cancel = document.createElement ("a");
+	    var cont_cancel = elems[1].firstChild.nodeValue;
+	    cancel.setAttribute ("href",
+				 "javascript:(function () {click_cancel("
+				 + cont_cancel + ")})()");
+	    cancel.setAttribute ("class", "cancel");
+	    cancel.appendChild (text_cancel);
+	    
+	    target_elem.appendChild (cancel);
+	    debug_out ("cont_cancel: " + cont_cancel);
+	    debug_out ("cancel: " + typeof (cancel.setAttribute));
+	    debug_out ("text_cancel: " + text_cancel);
 	}
     }
     xmlhttp.send (null);
+}
+
+function filter_element_nodes (nodes) {
+    var elems = [];
+
+    for (var i = 0; i < nodes.length; i ++) {
+	if (nodes[i].nodeType == 1) {
+	    elems.push (nodes[i]);
+	}
+    }
+
+    return elems;
 }
